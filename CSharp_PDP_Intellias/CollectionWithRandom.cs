8ab22e8566ff02b7a0f5c27a6733bc618ac5b641
Kateryna Shynkarenko <kshynkarenko@intellias.com>
2020-03-04 01:12:10 +0200

    collection with random numbers

---
+++ CSharp_PDP_Intellias/CollectionWithRandom.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PDP_Intellias
+{
+    public class CollectionWithRandom
+    {
+        
+        public List<int> numbers = new List<int>();
+
+        public void ListOfNumbers()
+        {
+            foreach (int x in numbers)
+            {
+                Console.WriteLine(x);
+            }
+        }
+       
+
+    //Масив в порядку збільшення
+    public void Growth()
+        {
+            
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count - 1; j++)
+                {
+                    if (numbers[j + 1] < numbers[j])
+                    {
+                        int c = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = c;
+                    }
+                }
+            }
+            Console.WriteLine("\nArray2: [{0}]", string.Join(", ", numbers));
+        }
+
+        //Масив в порядку зменшення-
+        public void Decrease()
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count - 1; j++)
+                {
+                    if (numbers[j + 1] > numbers[j])
+                    {
+                        int c = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = c;
+                    }
+                }
+            }
+            Console.WriteLine("\nArray3: [{0}]", string.Join(", ", numbers));
+        }
+    }
+}
+
---
+++ CSharp_PDP_Intellias/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PDP_Intellias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            CollectionWithRandom collection = new CollectionWithRandom();
+            collection.numbers.Add(random.Next(0, 10));
+            collection.numbers.Add(random.Next(0, 10));
+            collection.numbers.Add(random.Next(0, 10));
+            collection.numbers.Add(random.Next(0, 10));
+            collection.numbers.Add(random.Next(0, 10));
+            collection.numbers.Add(random.Next(0, 10));
+
+
+            collection.ListOfNumbers();
+            collection.Growth();
+            collection.Decrease();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
